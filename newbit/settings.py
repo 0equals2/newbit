9966e7a0d0159135916a99ee6140028b4d4f132f
0equals2 <wkgek93@gmail.com>
2019-08-27 17:53:35 +0900

    to release the app

--- newbit/settings.py
+++ newbit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$)q9@$w=5zn2tabfy-u%#2ck7zpbb$7coq=beqd5cs^t8rkbw%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
